7bb96db19f6f0a7bd17c03c8f8fa1cef7aac6552
sean <b05902066@ntu.edu.tw>
2019-10-01 20:37:30 +0800

    speed up code

--- joint_bilateral_filter.py
+++ joint_bilateral_filter.py
@@ -11,9 +11,12 @@ class Joint_bilateral_filter(object):
         self.sigma_r = sigma_r
         self.sigma_s = sigma_s
         self.r = 3*self.sigma_s
-        self.s_kernel = Joint_bilateral_filter.create_skernel(self.sigma_s, self.r)
-        self.w_cache = {}
+        self.s_kernel = Joint_bilateral_filter.create_skernel(self.sigma_s, self.r).reshape(-1)
 
+
+    def create_w_cache(self):
+        d = 255*255*2*self.sigma_r**2
+        return [np.exp(-float(i)/d) for i in range(255**3)]
 
     def joint_bilateral_filter(self, input_img, guidance):
 
@@ -25,21 +28,18 @@ class Joint_bilateral_filter(object):
         
         for i in range(output.shape[0]):
             for j in range(output.shape[1]):
-
                 print(i, j)
-                
                 patch_g = pad_guidance[i:i+2*self.r+1, j:j+2*self.r+1]
                 patch_i = pad_img[i:i+2*self.r+1, j:j+2*self.r+1]
 
                 r_kernel = self.create_rkernel(patch_g, i+self.r, j+self.r)
 
                 w = self.s_kernel*r_kernel
-
-                w = np.reshape(w, -1)
                 patch_i = np.reshape(patch_i, (-1, 3))
                 
-                v = w @ patch_i
-                v /= np.sum(w)
+                w = w.reshape((1, -1))
+                v = (w @ patch_i)/np.sum(w)
+
                 output[i, j] = v
 
         return output
@@ -66,17 +66,10 @@ class Joint_bilateral_filter(object):
 
         diff = np.reshape(diff, -1)
 
-        diff = np.array(list(map(self.findWeight, diff)))
-        # print(diff)
-        kernel = np.reshape(diff, (2*self.r+1, 2*self.r+1))
+
+        diff /= 255**2*2*self.sigma_r**2
+        diff *= -1
+        kernel = np.exp(diff)
 
         return kernel
-
-    def findWeight(self, a):
-
-        if str(a) not in self.w_cache:
-            self.w_cache[str(a)] = np.exp(-float(a)/255**2/(2*self.sigma_r**2))
-
-        return self.w_cache[str(a)]
-
         
